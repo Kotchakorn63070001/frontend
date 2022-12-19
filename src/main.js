63fbbe84603156e09a858b91f9b47c79fe98b387
it63070029 <73680368+it63070029@users.noreply.github.com>
2022-12-19 20:27:58 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'bulma/css/bulma.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 /* import specific icons */
 import { faBahtSign, faCarSide, faMarker, faPlus, faTrash, faUpload, faUserSecret } from '@fortawesome/free-solid-svg-icons'
 /* add icons to the library */
@@ -16,6 +17,7 @@ library.add(faMarker)
 library.add(faBahtSign)
 library.add(faUpload)
 library.add(faPlus)
+library.add(fas)
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
